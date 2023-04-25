68d6bde84a027a64205d7ec598630eb56e68f3ac
xEduardo <leon_roque@proton.me>
2023-04-25 15:40:33 -0600

    Agregando la resolucion del 2do ejercicio.

---
+++ Javascript/02-Sintaxis/index.js
@@ -0,0 +1,13 @@
+const lista = [
+    'Eduardo',
+    23,
+    true,
+    new Date(2000, 1, 24),
+    {
+        titulo: 'Eloquent JavaScript',
+        autor: 'Marijn Haverbeke',
+        fecha: '2018',
+        url: 'https://www.amazon.com.mx/Eloquent-Javascript-3rd-Introduction-Programming/dp/1593279507/ref=sr_1_2?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=javascript&qid=1681598197&s=books&sr=1-2'
+    }
+]
+console.log(lista)
