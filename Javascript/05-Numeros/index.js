d23c005309cadaf26e47a7346c2e6a61084a44cd
xEduardo <leon_roque@proton.me>
2023-04-26 19:05:00 -0600

    Agregando la resolucion del ejercicio numero 5 - Numbers (Numeros).

---
+++ Javascript/05-Numeros/index.js
@@ -0,0 +1,7 @@
+const alturaCentimetros = 167;
+const alturaMetros = alturaCentimetros / 100;
+const pesoKilogramos = 58.6;
+const alturaMetrosRedondeada = Math.ceil(alturaMetros)
+const pesoKilogramosRedondeado = Math.floor(pesoKilogramos)
+const siMaxValor = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
