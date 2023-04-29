6b8dc77f42db9e18cd24172390968eabe3588bfa
xEduardo <leon_roque@proton.me>
2023-04-29 15:09:13 -0600

    Agregando la resolucion 1 del ejercicio 7 - Sets y Objetos.

---
+++ Javascript/07-Sets/conjuntos.js
@@ -0,0 +1,13 @@
+const familia = new Set([
+  'Guadalupe',
+  'Cirilo',
+  'Marcos',
+  'Mariana',
+  'Naomi',
+  'Abdel',
+  'Marie',
+  'Eduardo'
+])
+familia.add('Eduardo')
+familia.add('JavaScript')
+console.log(familia)
